d3f2ab09e8f197477db9b81ae669542386d394f5
avantikasingh2110 <avantikasingh.2110@gmail.com>
2024-10-29 18:43:30 +0530

    Updated

--- script.js
+++ script.js
@@ -3,31 +3,48 @@ const birthday = document.getElementById("birthday");
 const result = document.getElementById("result");
 result.style.display = 'none';
 
+
 function calculateAge() {
     const birthdayValue = birthday.value;
     if (birthdayValue === "") {
         alert("Please enter your birthday");
     } else {
         const age = getAge(birthdayValue);
-        result.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
-        result.style.display = 'block';
+
+        if (age === null) {
+            alert("The child is not born yet.");
+            result.style.display = 'none';
+        } else {
+            result.innerText = `Your age is ${age.years} ${age.years > 1 ? "years" : "year"}, ${age.months} ${age.months > 1 ? "months" : "month"}, and ${age.days} ${age.days > 1 ? "days" : "day"} old.`;
+            result.style.display = 'block';
+        }
     }
 }
 
 function getAge(birthdayValue) {
     const currentDate = new Date();
     const birthdayDate = new Date(birthdayValue);
-    let age = currentDate.getFullYear() - birthdayDate.getFullYear();
-    const month = currentDate.getMonth() - birthdayDate.getMonth();
 
-    if (
-        month < 0 ||
-        (month === 0 && currentDate.getDate() < birthdayDate.getDate())
-    ) {
-        age--;
+    if (birthdayDate > currentDate) {
+        return null;
     }
 
-    return age;
+    let years = currentDate.getFullYear() - birthdayDate.getFullYear();
+    let months = currentDate.getMonth() - birthdayDate.getMonth();
+    let days = currentDate.getDate() - birthdayDate.getDate();
+
+    if (days < 0) {
+        months--;
+        const lastDayOfPrevMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+        days += lastDayOfPrevMonth;
+    }
+
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+
+    return { years, months, days };
 }
 
 btn.addEventListener("click", calculateAge);
